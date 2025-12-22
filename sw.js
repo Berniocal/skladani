dc46a88c08d599387f05f8a2865d3d60eb443905
Bernio <jendabernard@gmail.com>
2025-12-22 13:14:14 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js — Slovní kostky PWA offline
 // ↑ při každé úpravě SW ZVYŠ cache name -> vynutí se update
-const CACHE_NAME = 'slovni-kostky-v4-2025-12-22';
+const CACHE_NAME = 'skladani-slov-v4-2025-12-22';
 
 const ASSETS = [
   './',
