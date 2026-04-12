3d1c2208a927c65ae5767f1eff14f98aa387cba4
Bernio <jendabernard@gmail.com>
2026-04-12 13:30:08 +0200

    Change cache name for service worker
    Updated cache name to force service worker update.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js — Slovní kostky PWA offline
 // ↑ při každé úpravě SW ZVYŠ cache name -> vynutí se update
-const CACHE_NAME = 'skladani-slov-v4-2025-12-24';
+const CACHE_NAME = 'skladani-slov-v4-2025-12-25';
 
 const ASSETS = [
   './',
